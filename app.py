760816243422ccafb27b2d83db6ec21741dc30a1
FND <fnd@localhost.localdomain>
2015-08-28 12:12:21 +0200

    distinguished publication date and published flag
    in the process, introduced `Article` model - which is a good idea anyway
    for explicitness

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 import yaml
 
+from datetime import date
+
 from flask import Flask, render_template, abort, url_for
 
 
@@ -26,19 +28,45 @@ def articles():
     with open(STORE) as fh:
         store = yaml.load(fh)
     # add article IDs
-    articles = ((i, article) for i, article in enumerate(_retrieve("articles")))
+    articles = ((i, Article.from_dict(article))
+            for i, article in enumerate(_retrieve("articles")))
     return render_template("articles.html", title="articles", articles=articles)
 
 
 @app.route("/articles/<article_id>")
 def article(article_id):
     article = _retrieve("articles")[int(article_id)]
+    article = Article.from_dict(article)
     return render_template("article.html", title="article", article=article)
 
 
 @app.route("/authors")
 def authors():
     abort(501)
+
+
+class Article:
+
+    def __init__(self, title, authors, content, tags=None, pubdate=None, edits=None):
+        self.title = title
+        self.authors = authors
+        self.tags = tags or []
+        self.pubdate = pubdate
+        self.edits = edits or 0
+        self.content = content
+
+    @property
+    def published(self):
+        return date.today() > self.pubdate if self.pubdate else False
+
+    @staticmethod
+    def from_dict(article, article_id=None):
+        article = Article(article["title"], article["authors"],
+                article["content"], article.get("tags"),
+                article.get("pubdate"), article.get("edits"))
+        if article_id:
+            article.id = article_id
+        return article
 
 
 def _retrieve(category):
